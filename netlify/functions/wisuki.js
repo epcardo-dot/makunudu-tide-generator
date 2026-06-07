8ea47166d2e43a2ca08401954bbfdcfb009e8c99
epcardo-dot <epcardo@gmail.com>
2026-06-07 12:10:17 +0500

    Update wisuki.js

--- netlify/functions/wisuki.js
+++ netlify/functions/wisuki.js
@@ -1,10 +1,32 @@
 exports.handler = async function(event, context) {
 
-  return {
-    statusCode: 200,
-    body: JSON.stringify({
-      message: "Wisuki function is working!"
-    })
-  };
+  try {
+
+    const response = await fetch(
+      "https://wisuki.com/tide/463/kaafu-atoll"
+    );
+
+    const html = await response.text();
+
+    return {
+      statusCode: 200,
+      body: JSON.stringify({
+        success: true,
+        htmlLength: html.length,
+        first500Characters: html.substring(0, 500)
+      })
+    };
+
+  } catch (error) {
+
+    return {
+      statusCode: 500,
+      body: JSON.stringify({
+        success: false,
+        error: error.message
+      })
+    };
+
+  }
 
 };
